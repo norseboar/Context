61f911d54d81607c15dfa46be1d94d8be3b1ce63
norseboar <reed@outlook.com>
2015-04-12 15:06:37 -0700

    heavy refactoring, no functional changes
    Changed naming scheme, made sure variable/constant names had consistent
    style, and pulled content retrieving logic out of the main content
    script. This will allow retrieving logic to /also/ be used in context
    menu (before, it was tightly coupled with the original content script)

---
+++ content-retriever.js
@@ -0,0 +1,44 @@
+"use strict"
+// Module for retrieving content about a particular query and inserting it into
+// a hoverpane
+
+var context = context || {};
+
+context.contentRetriever = (function($) {
+  var getVoxContent = function (query) {
+    return context.cardstacks.getUrl(query);
+  };
+
+  var getWikipediaContent = function (query) {
+    // Wikipedia pages replace spaces in titles with underscores
+    query = query.replace(/\s+/gm, '_');
+    return context.WIKIPEDIA_PREFIX + query;
+  };
+
+  var getContextUrl = function (query) {
+    query = query.toLowerCase();
+    // If the term is associated with a cardstack, show the cardstack
+    var url = getVoxContent(query);
+    if (url) {
+      return url;
+    }
+    else {
+      return getWikipediaContent(query);
+    }
+  };
+
+  return {
+    // Gets a context URL based on the query, moves the hoverpane to the
+    // appropriate location, and inserts the iframe into the pane
+    // movement must be done at this time because moving the hoverpane could
+    // change its size, which affects what size the iframe can be
+    insertDataIntoPane: function(query, hp, jqElement) {
+      hp.reset();
+      hp.move(jqElement);
+      var url = getContextUrl(query);
+      var iframe = $('<iframe src="' + url + '" width="' + hp.width +
+          '" height="' + hp.height + '"></iframe>');
+      hp.appendContent(iframe, false);
+    }
+  };
+})(jQuery);
---
+++ content-script.js
@@ -0,0 +1,121 @@
+"use strict";
+(function ($) {
+
+  // Creates context pane, and creates a handler to update the context pane
+  // whenever text is selected
+  var hoverPane;
+  var init = function(){
+    hoverPane = new context.HoverPane();
+    var stacks = context.cardstacks.getKeywords();
+    // Wrap all cardstack-related terms in highlights that will pop cardstacks
+    $('p').highlight(context.cardstacks.getKeywords(), { element: 'span',
+      className: 'cardstack-highlight'});
+    $('.cardstack-highlight').click(function(event){
+      var element = $(event.currentTarget);
+      context.contentRetriever.insertDataIntoPane(element.text(), hoverPane,
+          element);
+    });
+
+    // whenever text is selected, query that text
+    $('body').mouseup(function () {
+      setTimeout(function () {
+        var selection = window.getSelection();
+        if(!selection) {
+          return;
+        };
+        var parentElement = $(selection.anchorNode.parentElement);
+        var element = $(selection.focusNode);
+        var query = getFullTextFromSelection(selection);
+        if(!isQueryValid(query, element)) {
+          return;
+        }
+        console.log("searching for " + query);
+        context.contentRetriever.insertDataIntoPane(query, hoverPane,
+            parentElement);
+      }, (400));
+    });
+  };
+
+  // Decides if a query is valid to search for
+  // Includes making sure a query isn't empty, isn't too large, and isn't in a text box
+  var isQueryValid = function(query, element){
+    var valid = true;
+    valid = valid && !query.isEmpty();
+    valid = valid && query.length <= context.MAX_QUERY_LENGTH;
+    valid = valid && query.split(/\s+/).length <= context.MAX_QUERY_WORDS;
+    valid = valid && !element.is(":text");
+    var close = element.closest("form");
+    valid = valid && element.closest("form").length === 0;
+    return valid;
+  };
+
+  // Strips out any punctuation that should end a word (whitespace, comma,
+  // colon, semicolon, period, question mark, exclamation mark)
+  String.prototype.removePunctuation = function(){
+    return this.replace(/[,:;.?!]/, '');
+  };
+
+  String.prototype.condenseWhitespace = function(){
+    return this.replace(/\s+/gm, ' ');
+  }
+
+  // Returns any words that are partially selected in addition to the full
+  // text of a selection
+  var getFullTextFromSelection = function (selection){
+    if(!selection){
+      return '';
+    }
+    var text = selection.toString();
+    if(!text){
+      return '';
+    }
+
+    var earlyIndex, lateIndex = 0;
+    var earlyText, lateText = "";
+    var anchorIsFirst = true;
+
+    var anchorPosition = selection.anchorNode.compareDocumentPosition(selection.focusNode);
+    if(anchorPosition) {
+      anchorIsFirst = (Node.DOCUMENT_POSITION_FOLLOWING & anchorPosition) ||
+        (Node.DOCUMENT_POSITION_CONTAINED_BY & anchorPosition);
+    }
+    else {
+      anchorIsFirst = selection.anchorOffset <= selection.focusOffset;
+    }
+    if(anchorIsFirst){
+      earlyIndex = selection.anchorOffset - 1;
+      earlyText = selection.anchorNode.nodeValue;
+      lateIndex = selection.focusOffset;
+      lateText = selection.focusNode.nodeValue;
+    }
+    else {
+      earlyIndex = selection.focusOffset - 1;
+      earlyText = selection.focusNode.nodeValue;
+      lateIndex = selection.anchorOffset;
+      lateText = selection.anchorNode.nodeValue;
+    }
+
+
+    var c = '';
+
+    // If the start of the selection leads with whitespace or punctuation, don't
+    // search farther forward
+    if(text[0].removePunctuation().trim()){
+      while(earlyText && (c = earlyText.charAt(earlyIndex--).removePunctuation().trim())){
+        text = c.concat(text);
+      }
+    }
+
+    // Same for the end of the selection trailing with whitespace or punctuation
+    if(text[(text.length -1)].removePunctuation().trim()){
+      while(lateText && (c = lateText.charAt(lateIndex++).removePunctuation().trim())){
+        text = text.concat(c);
+      }
+    }
+
+    return text.removePunctuation().condenseWhitespace().trim();
+  };
+
+  init();
+
+})(jQuery);
